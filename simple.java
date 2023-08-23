992da240bc2771182a17eb580ef24b7760c4480b
Lavine Oluoch <lavineoluoch@gmail.com>
2023-08-23 15:00:18 -0500

    Create simple.java
    Writing Simple Programs

---
+++ simple.java
@@ -0,0 +1,28 @@
+public class HelloWorld{
+  public static void main(String[] args){
+   System.out.println("Hello World!");
+     System.out.println();
+//SpellingChequer
+    System.out.println("Eye halve a spelling chequer");
+    System.out.println("It came with my pea sea");
+    System.out.println("It plainly marques four my revue");
+    System.out.println("Miss steaks eye kin knot sea.");
+    System.out.println();
+//Print Diamond
+System.out.println("  **  ");
+System.out.println(" **** ");
+System.out.println("******");
+System.out.println("******");
+System.out.println(" **** ");
+System.out.println("  **  ");
+System.out.println();
+//QuotesAndSlashes
+System.out.println("  /\\  ");
+System.out.println(" /\"\"\\ ");
+System.out.println("/\"\"\"\"\\");
+System.out.println("\\\"\"\"\"/");
+System.out.println(" \\\"\"/ ");
+System.out.println("  \\/  ");
+
+  }
+}
