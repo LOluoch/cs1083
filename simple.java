fe054bd6fab9e0115c5e7f4e4a3f812aa450ed7f
Lavine Oluoch <lavineoluoch@gmail.com>
2023-08-23 15:22:01 -0500

    Update simple.java
    Clean up syntax and add initials

--- simple.java
+++ simple.java
@@ -1,7 +1,7 @@
 public class HelloWorld{
   public static void main(String[] args){
    System.out.println("Hello World!");
-     System.out.println();
+    System.out.println();
 //SpellingChequer
     System.out.println("Eye halve a spelling chequer");
     System.out.println("It came with my pea sea");
@@ -9,20 +9,31 @@ public class HelloWorld{
     System.out.println("Miss steaks eye kin knot sea.");
     System.out.println();
 //Print Diamond
-System.out.println("  **  ");
-System.out.println(" **** ");
-System.out.println("******");
-System.out.println("******");
-System.out.println(" **** ");
-System.out.println("  **  ");
-System.out.println();
+    System.out.println("  **  ");
+    System.out.println(" **** ");
+    System.out.println("******");
+    System.out.println("******");
+    System.out.println(" **** ");
+    System.out.println("  **  ");
+    System.out.println();
 //QuotesAndSlashes
-System.out.println("  /\\  ");
-System.out.println(" /\"\"\\ ");
-System.out.println("/\"\"\"\"\\");
-System.out.println("\\\"\"\"\"/");
-System.out.println(" \\\"\"/ ");
-System.out.println("  \\/  ");
+    System.out.println("  /\\  ");
+    System.out.println(" /\"\"\\ ");
+    System.out.println("/\"\"\"\"\\");
+    System.out.println("\\\"\"\"\"/");
+    System.out.println(" \\\"\"/ ");
+    System.out.println("  \\/  ");
+    System.out.println();
+//Initials
+    System.out.println("Lab 0 written by XYZ");
+    System.out.println();
 
+    System.out.println("X     X  Y     Y ZZZZZZZ");
+    System.out.println(" X   X    Y   Y       Z");
+    System.out.println("  X X      Y Y       Z");
+    System.out.println("   X        Y       Z");
+    System.out.println("  X X       Y      Z");
+    System.out.println(" X   X      Y     Z");
+    System.out.println("X     X     Y    ZZZZZZZ");
   }
 }
