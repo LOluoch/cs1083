cbf2b64491c6db2141445d79578b630c0a401a46
Lavine Oluoch <lavineoluoch@gmail.com>
2023-08-24 12:14:07 -0500

    Update box.java
    introduced Class Constants

--- box.java
+++ box.java
@@ -1,25 +1,28 @@
 public class printBox {
+    public static final int BOX_HEIGHT = 10;
+    public static final int BOX_WIDTH = 18;
+    
     public static void main(String[] args) {
-       Box();
+       box();
 }
 
 public static void box(){ // 12 by 7 box
     printAsterisks();
-    for (int i=1; i<=5; i++)
+    for (int i=1; i<=(BOX_HEIGHT-2); i++)
         printAsteriskSpacestAsterisk();
     printAsterisks();
     
 }
 
 public static void printAsterisks(){ // Print top and bottom line of box
-    for(int i=1;i<=12;i++)
+    for(int i=1;i<=BOX_WIDTH;i++)
          System.out.print("*");
     System.out.println( );
 }
 
 public static void printAsteriskSpacestAsterisk(){ // print 1 row of box
     System.out.print("*");
-    for (int i=1; i<=10;i++)
+    for (int i=1; i<=(BOX_WIDTH-2);i++)
         System.out.print(" ");
     System.out.print("*");
     System.out.println( );
