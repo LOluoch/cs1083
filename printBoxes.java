b4ee415490645b5374e21b79e1d5a9853aa9e83d
Lavine Oluoch <lavineoluoch@gmail.com>
2023-08-28 08:07:49 -0500

    Update printBoxes.java
    Add comments

--- printBoxes.java
+++ printBoxes.java
@@ -1,18 +1,24 @@
-public class Stars {
+public class printBoxes {
    public static void main(String[] args) {
       drawBox(15,20);
    }
 
    public static void drawBox(int height, int width) {
+// print a box with a specified height and width
+
+    // 1. Print a line with width stars.
        writeStars(width);
+// 2. Do the following height - 2 times.
        for (int i = 1; i<=height -2;i++){
-           System.out.print("x");
+ // A. Print a line with a star, width - 2 spaces, and a star.
+           System.out.print("*");
            for (int x = 1; x <=width-2; x++){
            System.out.print(" ");
            }
-           System.out.print("x");
+           System.out.print("*");
         System.out.println();
        }
+// 3. Print a line with width stars.
        writeStars(width);
    }
    
